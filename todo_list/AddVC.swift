c142dd2f4ccb7f5ce49196d1119d47d4755e4e08
ruslanvs <ruslanvs@yahoo.com>
2018-03-16 08:06:29 -0700

    Initial view controllers setup

---
+++ todo_list/AddVC.swift
@@ -0,0 +1,37 @@
+//
+//  AddVC.swift
+//  todo_list
+//
+//  Created by Ruslan Suvorov on 3/16/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import UIKit
+
+class AddVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("here")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
